27458d5d522530e9cd38aa5c306e45271bbe18ae
Purnomo, Christoph <christoph.purnomo@here.com>
2017-11-19 13:54:22 +0100

    Text reviewed and modified. added ALEXA_APP prompt

--- instructions.js
+++ instructions.js
@@ -2,25 +2,25 @@
  * Created by William on 5/10/2017.
  */
 module.exports = {
-    "START_SKILL" : " Willkommen bei Deutschtutor. Willst Du zuerst dein Sprachlevel setzen oder sofort mit dem Training beginnen?",
+    "START_SKILL" : "Willkommen bei Deutschtutor. Willst Du zuerst dein Sprachlevel setzen oder sofort mit dem Training beginnen?",
     "START_SKILL_REPROMPT" : "Setze dein Sprachlevel oder beginne sofort mit dem Training",
-    "START_TRAINING" : "An welchem Training möchtest du teilnehmen? Aussprache oder Hörverstehen?"
-    "START_TRAINING_REPROMPT" : "Wir bieten Aussprache und Hörverstehen. Welchen Bereich möchtest du trainieren?",
-    "CHOOSE_TRAINING": " Welches Training willst du starten? ",
-    "CHANGE_LEVEL" : " Welches Level willst du. Einfach, Normal, oder Schwierig? ",
-    "CHANGE_LEVEL_REPROMPT" : " Einfach, Normal oder Schwierig?",
-    "REPEAT_AFTER_ME" : " Sprich mir nach: ",
-    "YOUR_LEVEL_IS" : " Dein Level ist ",
-    "PLEASE_ASWER_FOLOWING_QUESTION" : " Bitte folgende Fragen beantworten: ",
-    "IS_CORRECT" : " ist richtig",
-    "IS_FALSE" : " ist falsch",
-    "BYE" : " auf Wiedersehen",
-    "YOU_HAVE" : " du hast ",
-    "POINT" : " Punkte ",
-    "SCORED" : " erreicht ",
-    "FROM" : " von ",
+    "CHOOSE_TRAINING": "An welchem Training möchtest du teilnehmen? Aussprache oder Hörverstehen?",
+    "CHOOSE_TRAINING_REPROMPT" : "Wir bieten Aussprache und Hörverstehen. Welchen Bereich möchtest du trainieren?",
+    "CHANGE_LEVEL" : "Welches Level? Einfach, Normal, oder Schwierig?",
+    "CHANGE_LEVEL_REPROMPT" : "Einfach, Normal oder Schwierig?",
+    "REPEAT_AFTER_ME" : "Sprich mir nach:",
+    "YOUR_LEVEL_IS" : "Dein Level ist",
+    "PLEASE_ANSWER_FOLLOWING_QUESTION" : "Bitte folgende Fragen beantworten:",
+    "IS_CORRECT" : "ist richtig",
+    "IS_FALSE" : "ist falsch",
+    "BYE" : "Auf Wiedersehen",
+    "YOU_HAVE" : "Du hast",
+    "POINT" : "Punkten",
+    "SCORED" : "erreicht",
+    "FROM" : "von",
+    "ALEXA_APP" : "Die korrekten Lösungen mit weiteren Begriffserläuterungen habe ich an Deine Alexa App geschickt.",
     "CONVERSATION_TRAINING" : "Aussprache Training",
-    "LEVEL" : " Level ",
+    "LEVEL" : " Level",
     "WELCOME": "Willkommen",
-    "WELCOME_HELP" : 'Weiter mit:\r\n"Aussprache" (pronunciation)\r\n"Hörverstehen" (listening)\r\n"Gespräch" (conversation)\r\n"Level setzen"'
+    "WELCOME_HELP" : 'Weiter mit:\r\n"Aussprache" (pronunciation)\r\n"Hörverstehen" (listening)\r\n"Level setzen"'
 }
